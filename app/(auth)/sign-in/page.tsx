c713583989de84eb79f14a28de86be38f599e0dd
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-24 10:52:49 +0300

    Auth Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
+import AuthForm from "@/components/form/AuthForm";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <AuthForm
+        formType="SIGN_IN"
+        schema={SignInSchema}
+        defaultValues={{ email: "", password: "" }}
+        onSubmit={(data) =>
+          Promise.resolve({
+            success: true,
+            data,
+          })
+        }
+      />
+    </div>
+  );
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import AuthForm from "@/components/form/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <AuthForm
+        formType="SIGN_UP"
+        schema={SignUpSchema}
+        defaultValues={{ email: "", password: "", name: "", username: "" }}
+        onSubmit={(data) =>
+          Promise.resolve({
+            success: true,
+            data,
+          })
+        }
+      />
+    </div>
+  );
 };
 
 export default SignUp;
