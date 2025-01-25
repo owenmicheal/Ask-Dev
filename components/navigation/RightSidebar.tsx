2c78f57bc847692f488e5a5623b669e6cc501dee
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-01-25 17:06:00 +0300

    small fix on right sidebar

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -32,7 +32,7 @@ const RightSidebar = () => {
     <section
       className="pt-36 custom-scrollbar background-light900_dark200 light-border
     sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto 
-    border-l p-6 shadow-light-300 dark:shadow-none"
+    border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden"
     >
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
